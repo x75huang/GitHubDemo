fd89eb26364eca2d825c5925848f2db21b0e43d7
Lynne <Lynne@192.168.7.100>
2015-04-22 13:51:30 -0400

    Add "Second Change"

--- GitHubDemo/src/GitHub.java
+++ GitHubDemo/src/GitHub.java
@@ -4,7 +4,7 @@ public class GitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("The first change!!!");
-		System.out.println("The second change!!!");
+		System.out.println("The second change!!!!!");
 	}
 
 }
